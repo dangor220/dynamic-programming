458227f7d63a4df515aa9a0850dad3df0b23fe0a
Danil Gordeev <dan.gor220@yandex.ru>
2023-10-23 19:52:37 +0300

    feat: complete conditionaloptimization function

--- script.js
+++ script.js
@@ -9,54 +9,43 @@ let data = [
 ];
 let q = 0.5; // Коэффициент прироста времени при более высоком уровне освоения
 
-// Объявление функции
+// Объявление функции условной оптимизации
 function conditionalOptimization(data, q) {
-  let conditionalMaximums = {}, // объект с условными максимумами
-    k = data.length - 1; // число шагов начиная с последнего
+  const conditionalMaximums = {};    // Объект с условными максимумами
+  const k = data.length - 1;         // Число шагов k
+ 
+  for (let step = k; step >= 1; step--) {  // Обходим каждый шаг, начиная с конца
+    conditionalMaximums[`Z_${step}`] = {}; // Создаем объект с данными о условных максимумах при различном времени
+    for (const T of data[0]) {             // Обход массива с данными о времени
+      for (let t = T; t >= 0; t--) {       // Получаем допустимые значения t
+        const currentLevel = data[step][t - 1] || 0;    // Получаем уровень освоения при заданном t или 0, если значение отсутствует
 
-  for (let step = k; step >= 1; step--) {
-    conditionalMaximums[`Z_${step}`] = {};
-    for (let T of data[0]) {
-      for (let t = T; t >= 0; t--) {
-        let currentLevel = data[step][t - 1] || 0;
-
-        if (conditionalMaximums[`Z_${step + 1}`]) {
-          let Q = (t - data[0][0]) * q;
-          if (Q < 0) Q = 0;
-
-          let prevLevel = T - t + Q;
-
-          if (!conditionalMaximums[`Z_${step + 1}`][prevLevel]) {
-            prevLevel = Math.floor(prevLevel);
-          }
-
-          let sum = null;
-          if (prevLevel === 0) {
-            sum = currentLevel + 0;
-          } else {
-            sum =
-              currentLevel + conditionalMaximums[`Z_${step + 1}`][prevLevel][0];
-          }
-
-          if (
-            conditionalMaximums[`Z_${step}`][T] &&
-            conditionalMaximums[`Z_${step}`][T][0] > sum
-          ) {
-            continue;
-          } else {
+        if (conditionalMaximums[`Z_${step + 1}`]) {     // Если существует предыдущий условный максимум
+          const Q = Math.max((t - data[0][0]) * q, 0);  // Находим дополнительное время Q >= 0 при заданном коэффициенте q
+                                                        // (при условии повышенного уровня освоения предыдущего занятия)
+          let prevLevel = T - t + Q;     // Получаем общее время
+          // Если условный максимум в предыдущем значении не достигается, округляем до целого числа в меньшую сторону
+          if (!conditionalMaximums[`Z_${step + 1}`][prevLevel]) prevLevel = Math.floor(prevLevel);
+          // Если общее время равняется нулю, то предыдущий условный максимум равен нулю
+          const sum = prevLevel ===  0 ? currentLevel : currentLevel + conditionalMaximums[`Z_${step + 1}`][prevLevel][0];
+           // Добавляем значение в объекст услоовных максимумов
+          if (!conditionalMaximums[`Z_${step}`][T] || conditionalMaximums[`Z_${step}`][T][0] < sum) {
             conditionalMaximums[`Z_${step}`][T] = [sum, t];
           }
         } else {
+          // Добавляем значение в объекст услоовных максимумов
           conditionalMaximums[`Z_${step}`][t] = [currentLevel, t];
         }
       }
     }
   }
+  // Возвращаем объект условных максимумов
   return conditionalMaximums;
 }
 
+
 function unconditionalOptimization(conditionalMaximums) {
-  return conditionalMaximums;
+  console.log(conditionalMaximums); ;
 }
 
 unconditionalOptimization(conditionalOptimization(data, q));
